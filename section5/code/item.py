4a8dee47ffaff0122b446ceea4549fa041dc4f84
kqiu10 <qiukan97@gmail.com>
2021-01-14 15:13:26 -0500

    implement delete

---
+++ section5/code/item.py
@@ -0,0 +1,78 @@
+from flask_restful import Resource, reqparse
+import sqlite3
+from flask_jwt import jwt_required
+
+class Item(Resource):
+    TABLE_NAME ='items'
+
+    parser = reqparse.RequestParser()
+    parser.add_argument("price",
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!")
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found"}, 404
+
+
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect("section5.db")
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {"item": {"name": row[0], "price": row[1]}}
+
+
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': "An item with same name '{}' already exists.".format(name)}, 400
+        data = Item.parser.parse_args()
+        item = {"name": name, "price": data['price']}
+        connection = sqlite3.connect("section5.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        result = cursor.execute(query, (item['name'], item['price']))
+        row = result.fetchone()
+        connection.close()
+
+        return item, 201
+
+    def delete(self, name):
+        connection = sqlite3.connect("section5.db")
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x : x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
